5b75af8bdd03816a3a732f33ff8f8ba05d4762e6
Matthew Little <zone117x@gmail.com>
2022-03-09 18:29:16 +0100

    fix: add taproot to required softforks

--- p2pool/networks/bitcoin_testnet.py
+++ p2pool/networks/bitcoin_testnet.py
@@ -17,7 +17,7 @@ BOOTSTRAP_ADDRS = 'forre.st liteco.in 78.158.149.247'.split(' ')
 ANNOUNCE_CHANNEL = '#p2pool-alt'
 VERSION_CHECK = lambda v: None if 100000 <= v else 'Bitcoin version too old. Upgrade to 0.11.2 or newer!' # not a bug. BIP65 support is ensured by SOFTFORKS_REQUIRED
 VERSION_WARNING = lambda v: None
-SOFTFORKS_REQUIRED = set(['bip65', 'csv', 'segwit'])
+SOFTFORKS_REQUIRED = set(['bip65', 'csv', 'segwit', 'taproot'])
 MINIMUM_PROTOCOL_VERSION = 3200
 SEGWIT_ACTIVATION_VERSION = 15
 BLOCK_MAX_SIZE = 1000000
